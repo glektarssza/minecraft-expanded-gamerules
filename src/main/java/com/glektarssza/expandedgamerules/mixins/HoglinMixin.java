9382a89ab3a8fc246b028cbb72da56e8b55e2dc7
G'lek Tarssza <glek@glektarssza.com>
2023-02-15 13:48:46 -0700

    Started adding a Hoglin mixin.

---
+++ src/main/java/com/glektarssza/expandedgamerules/mixins/HoglinMixin.java
@@ -0,0 +1,10 @@
+package com.glektarssza.expandedgamerules.mixins;
+
+import org.spongepowered.asm.mixin.Mixin;
+
+import net.minecraft.entity.monster.HoglinEntity;
+
+@Mixin(HoglinEntity.class)
+public class HoglinMixin {
+
+}
