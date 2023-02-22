184ba311a36f84e10c21dda922876380d813b799
G'lek Tarssza <glek@glektarssza.com>
2023-02-21 19:14:52 -0700

    Starting to build out the Fabric port.

--- src/main/java/com/glektarssza/expandedgamerules/ExpandedGamerules.java
+++ src/main/java/com/glektarssza/expandedgamerules/ExpandedGamerules.java
@@ -1,46 +1,17 @@
 package com.glektarssza.expandedgamerules;
 
-import net.minecraftforge.fml.common.Mod;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
-import com.glektarssza.expandedgamerules.gamerules.DisableEndermanTeleportGamerule;
-import com.glektarssza.expandedgamerules.gamerules.DisableShulkerTeleportGamerule;
-import com.glektarssza.expandedgamerules.gamerules.DisableTargetingPlayersGamerule;
+import net.fabricmc.api.ModInitializer;;
 
-/**
- * The root mod class.
- */
-@Mod(ExpandedGamerules.MOD_ID)
-public class ExpandedGamerules {
-    /**
-     * The ID of the mod.
-     */
+public class ExpandedGamerules implements ModInitializer {
     public static final String MOD_ID = "expandedgamerules";
 
-    /**
-     * The logger to use for the mod.
-     */
-    public static final Logger LOGGER = LogManager.getLogger();
+    public static final Logger LOGGER = LogManager.getLogger(MOD_ID);
 
-    /**
-     * The registry of gamerules.
-     */
-    public static final GameruleRegistry GAMERULE_REGISTRY = new GameruleRegistry();
-
-    /**
-     * Create a new instance.
-     */
-    public ExpandedGamerules() {
-        registerGamerules();
-    }
-
-    /**
-     * Register this mod's gamerules.
-     */
-    public void registerGamerules() {
-        new DisableTargetingPlayersGamerule().register(GAMERULE_REGISTRY);
-        new DisableEndermanTeleportGamerule().register(GAMERULE_REGISTRY);
-        new DisableShulkerTeleportGamerule().register(GAMERULE_REGISTRY);
+    @Override
+    public void onInitialize() {
+        // TODO: Implement mod initialization
     }
 }
