59901e9557ef850aa3d4e4866f4be1060ec2378f
G'lek Tarssza <glek@glektarssza.com>
2023-12-28 19:56:33 -0700

    Changed method casing.

--- common/src/main/java/com/glektarssza/expandedgamerules/CommonClass.java
+++ common/src/main/java/com/glektarssza/expandedgamerules/CommonClass.java
@@ -5,7 +5,7 @@ import net.minecraft.core.registries.BuiltInRegistries;
 import net.minecraft.world.item.Items;
 
 public class CommonClass {
-    public static void Init() {
+    public static void init() {
         Constants.LOG.info("Hello from Common init on {}! we are currently in a {} environment!",
                 Services.PLATFORM.getPlatformName(), Services.PLATFORM.getEnvironmentName());
         Constants.LOG.info("The ID for diamonds is {}", BuiltInRegistries.ITEM.getKey(Items.DIAMOND));
