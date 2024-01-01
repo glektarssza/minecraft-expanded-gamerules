bc5d1cffde4a133f05a6caf8788204646b323a2b
G'lek Tarssza <glek@glektarssza.com>
2024-01-01 12:51:07 -0700

    Added lots of doc comments.

--- common/src/main/java/com/glektarssza/expandedgamerules/Constants.java
+++ common/src/main/java/com/glektarssza/expandedgamerules/Constants.java
@@ -3,8 +3,22 @@ package com.glektarssza.expandedgamerules;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+/**
+ * Common mod constants.
+ */
 public class Constants {
+    /**
+     * The ID of the mod.
+     */
     public static final String MOD_ID = "expandedgamerules";
+
+    /**
+     * The name of the mod.
+     */
     public static final String MOD_NAME = "Expanded Gamerules";
+
+    /**
+     * The mod logger.
+     */
     public static final Logger LOG = LoggerFactory.getLogger(MOD_NAME);
 }
