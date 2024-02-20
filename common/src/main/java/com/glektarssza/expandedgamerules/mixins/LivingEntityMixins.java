f853cf4b25f3c34c4975846f8acfc8435d5ad124
G'lek Tarssza <glek@glektarssza.com>
2024-02-20 14:49:27 -0700

    Forgot to set method name.

--- common/src/main/java/com/glektarssza/expandedgamerules/mixins/LivingEntityMixins.java
+++ common/src/main/java/com/glektarssza/expandedgamerules/mixins/LivingEntityMixins.java
@@ -14,14 +14,14 @@ import net.minecraft.world.entity.player.Player;
  * Mixins relating to living entities.
  */
 @Mixin(LivingEntity.class)
-public class LivingEntityMixins {
+public final class LivingEntityMixins {
     /**
      * Check whether a living entity can attack another living entity.
      *
      * @param entity The entity to check if this living entity can attack.
      * @param ci     The callback information.
      */
-    @Inject(at = @At("HEAD"), method = "", cancellable = true)
+    @Inject(at = @At("HEAD"), method = "canAttack(Lnet/minecraft/world/entity/LivingEntity;)Z", cancellable = true)
     public static void canAttack(LivingEntity entity, CallbackInfoReturnable<Boolean> ci) {
         if (entity instanceof Player && GameruleUtilities.getBooleanGamerule(null, "disableTargetingPlayers")) {
             ci.setReturnValue(false);
