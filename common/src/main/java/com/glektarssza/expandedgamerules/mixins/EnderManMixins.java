554ce88128edb642b8543082383adab99ec9cd9f
G'lek Tarssza <glek@glektarssza.com>
2024-02-20 21:28:10 -0700

    Bunch of small changes.

--- common/src/main/java/com/glektarssza/expandedgamerules/mixins/EnderManMixins.java
+++ common/src/main/java/com/glektarssza/expandedgamerules/mixins/EnderManMixins.java
@@ -7,21 +7,33 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 import com.glektarssza.expandedgamerules.GameruleUtilities;
 
+import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.entity.monster.EnderMan;
+import net.minecraft.world.level.Level;
 
 /**
  * Mixins relating to Endermen.
  */
 @Mixin(EnderMan.class)
-public final class EnderManMixins {
+public abstract class EnderManMixins extends EnderMan {
+    /**
+     * Required to make Java be quiet.
+     *
+     * @param entityType The entity type.
+     * @param level      The level.
+     */
+    public EnderManMixins(EntityType<? extends EnderMan> entityType, Level level) {
+        super(entityType, level);
+    }
+
     /**
      * Attempt to teleport the Enderman.
      *
      * @param ci The callback information.
      */
     @Inject(at = @At("HEAD"), method = "teleport(DDD)Z", cancellable = true)
-    public static void teleport(double x, double y, double z, CallbackInfoReturnable<Boolean> ci) {
-        if (GameruleUtilities.getBooleanGamerule(null, "disableEndermanTeleport")) {
+    public void teleport(EnderMan entity, double x, double y, double z, CallbackInfoReturnable<Boolean> ci) {
+        if (GameruleUtilities.getBooleanGamerule(this.level(), "disableShulkerTeleport")) {
             ci.setReturnValue(false);
             return;
         }
--- common/src/main/java/com/glektarssza/expandedgamerules/mixins/LivingEntityMixins.java
+++ common/src/main/java/com/glektarssza/expandedgamerules/mixins/LivingEntityMixins.java
@@ -7,14 +7,26 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 import com.glektarssza.expandedgamerules.GameruleUtilities;
 
+import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.level.Level;
 
 /**
  * Mixins relating to living entities.
  */
 @Mixin(LivingEntity.class)
-public final class LivingEntityMixins {
+public abstract class LivingEntityMixins extends LivingEntity {
+    /**
+     * Required to make Java be quiet.
+     *
+     * @param entityType The entity type.
+     * @param level      The level.
+     */
+    protected LivingEntityMixins(EntityType<? extends LivingEntity> entityType, Level level) {
+        super(entityType, level);
+    }
+
     /**
      * Check whether a living entity can attack another living entity.
      *
@@ -22,8 +34,8 @@ public final class LivingEntityMixins {
      * @param ci     The callback information.
      */
     @Inject(at = @At("HEAD"), method = "canAttack(Lnet/minecraft/world/entity/LivingEntity;)Z", cancellable = true)
-    public static void canAttack(LivingEntity entity, CallbackInfoReturnable<Boolean> ci) {
-        if (entity instanceof Player && GameruleUtilities.getBooleanGamerule(null, "disableTargetingPlayers")) {
+    public void canAttack(LivingEntity entity, CallbackInfoReturnable<Boolean> ci) {
+        if (entity instanceof Player && GameruleUtilities.getBooleanGamerule(this.level(), "disableTargetingPlayers")) {
             ci.setReturnValue(false);
             return;
         }
