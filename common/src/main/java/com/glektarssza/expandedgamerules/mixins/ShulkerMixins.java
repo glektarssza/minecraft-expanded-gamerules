b4a72b8a52bbd8ab3cf6a5a1b448bb066898a358
G'lek Tarssza <glek@glektarssza.com>
2024-03-09 14:46:51 -0700

    Changed `level()` to `getLevel()`

--- common/src/main/java/com/glektarssza/expandedgamerules/mixins/ShulkerMixins.java
+++ common/src/main/java/com/glektarssza/expandedgamerules/mixins/ShulkerMixins.java
@@ -10,18 +10,16 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 import com.glektarssza.expandedgamerules.GameruleUtilities;
 
 import net.minecraft.world.entity.EntityType;
-import net.minecraft.world.entity.VariantHolder;
 import net.minecraft.world.entity.animal.AbstractGolem;
 import net.minecraft.world.entity.monster.Enemy;
 import net.minecraft.world.entity.monster.Shulker;
-import net.minecraft.world.item.DyeColor;
 import net.minecraft.world.level.Level;
 
 /**
  * Mixins relating to Shulkers.
  */
 @Mixin(Shulker.class)
-public abstract class ShulkerMixins extends AbstractGolem implements VariantHolder<Optional<DyeColor>>, Enemy {
+public abstract class ShulkerMixins extends AbstractGolem implements Enemy {
     /**
      * Make Java Happy™.
      *
@@ -40,7 +38,7 @@ public abstract class ShulkerMixins extends AbstractGolem implements VariantHold
     @Inject(at = @At("HEAD"), method = "teleportSomewhere()Z", cancellable = true)
     public void teleportSomewhere(CallbackInfoReturnable<Boolean> ci) {
         var self = (Shulker) (Object) this;
-        if (GameruleUtilities.getBooleanGamerule(self.level(), "disableEndermanTeleport")) {
+        if (GameruleUtilities.getBooleanGamerule(self.getLevel(), "disableEndermanTeleport")) {
             ci.setReturnValue(false);
         }
     }
