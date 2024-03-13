dcba32a35184a432e0e38eb1f6c489f96e76fc1b
G'lek Tarssza <glek@glektarssza.com>
2024-03-13 03:21:57 -0600

    `findFirst` didn't exist in Java 8.

--- common/src/main/java/com/glektarssza/expandedgamerules/platform/Services.java
+++ common/src/main/java/com/glektarssza/expandedgamerules/platform/Services.java
@@ -26,8 +26,11 @@ public class Services {
     public static <T> T load(Class<T> clazz) {
         Constants.LOG.debug("Loading platform-specific service {}...", clazz);
         final T loadedService = ServiceLoader.load(clazz)
-                .findFirst()
-                .orElseThrow(() -> new NullPointerException("Failed to load service for " + clazz.getName()));
+                .iterator()
+                .next();
+        if (loadedService == null) {
+            new NullPointerException("Failed to load service for " + clazz.getName());
+        }
         Constants.LOG.debug("Loaded {} for platform-specific service {}", loadedService, clazz);
         return loadedService;
     }
