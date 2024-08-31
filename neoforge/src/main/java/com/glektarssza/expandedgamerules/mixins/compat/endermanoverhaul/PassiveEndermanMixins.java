ac844714d38d50f675a483c41163d8a8a36e493f
G'lek Tarssza <glek@glektarssza.com>
2024-08-31 16:13:56 -0600

    Moved around compat mixins.

---
+++ neoforge/src/main/java/com/glektarssza/expandedgamerules/mixins/compat/endermanoverhaul/PassiveEndermanMixins.java
@@ -0,0 +1,43 @@
+package com.glektarssza.expandedgamerules.mixins.compat.endermanoverhaul;
+
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+import com.glektarssza.expandedgamerules.GameruleUtilities;
+
+import net.minecraft.world.entity.EntityType;
+import net.minecraft.world.entity.monster.EnderMan;
+import net.minecraft.world.level.Level;
+import tech.alexnijjar.endermanoverhaul.common.entities.base.BaseEnderman;
+import tech.alexnijjar.endermanoverhaul.common.entities.base.PassiveEnderman;
+
+/**
+ * Mixins for the base passive Enderman class from Enderman Overhaul.
+ */
+@Mixin(value = PassiveEnderman.class, remap = false)
+public abstract class PassiveEndermanMixins extends BaseEnderman {
+    /**
+     * Make Java Happy™.
+     *
+     * @param entityType The type of the entity being created.
+     * @param level      The game level.
+     */
+    public PassiveEndermanMixins(EntityType<? extends EnderMan> entityType, Level level) {
+        super(entityType, level);
+    }
+
+    /**
+     * Check whether the Enderman can teleport.
+     *
+     * @param ci The callback information.
+     */
+    @Inject(at = @At("HEAD"), method = "canTeleport()Z", cancellable = true, remap = false)
+    public void canTeleport(CallbackInfoReturnable<Boolean> ci) {
+        if (GameruleUtilities.getBooleanGamerule(this.level(), "disableEndermanTeleport")) {
+            ci.setReturnValue(false);
+            return;
+        }
+    }
+}
